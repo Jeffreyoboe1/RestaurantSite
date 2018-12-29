d94a2046d861a84bc65a4758af9c9d02c4fd5e46
Jeff Padgett <jeff_padgett@hotmail.com>
2018-12-29 01:18:25 -0600

    added validation functions for name, phone, email

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -4,14 +4,62 @@ Date Created: Dec. 29, 2018
 Date modified:
 */
 
-function isEmail() {
-var email = document.getElementById("email").value;
-  if (email.indexOf("@") == -1) {
-    alert("false! no @ sign");
+function validateForm() {
+  clearErrors();
+
+  if (!nameValid()) {
+    return false;
+  }
+  if (!emailValid()){
+    return false;
+  }
+  if (!phoneValid()) {
     return false;
   }
 
-  alert("submitted");
+  alert("Form Validated!");
+}
+
+
+
+function nameValid() {
+  var name = document.getElementById("name").value;
+
+  if (name == "" || name.length<2) {
+    alert("Enter your name.")
+    document.getElementById("name").parentElement.className="hasError form-group";
+    document.getElementById("name").focus();
+    return false;
+  }
+  return true;
+}
+
+function emailValid() {
+  var email = document.getElementById("email").value;
+  if (email.indexOf("@") == -1 || email[0] == "@" || email[0] == "." || email[email.length-1] == "." || email[email.length-1] == "@") {
+    alert("You did not enter a valid email address.");
+    document.getElementById("email").parentElement.className="hasError form-group";
+    document.getElementById("email").focus();
+    return false;
+  }
   return true;
 
+}
+
+function phoneValid() {
+  var phone = document.getElementById("phone").value;
+
+  if (phone == "" || phone.length<10) {
+    alert("Enter a valid phone number with an area code.")
+    document.getElementById("phone").parentElement.className="hasError form-group";
+    document.getElementById("phone").focus();
+    return false;
+  }
+  return true;
+}
+
+function clearErrors() {
+  document.getElementById("email").parentElement.className="form-group";
+  document.getElementById("name").parentElement.className="form-group";
+  document.getElementById("phone").parentElement.className="form-group";
 }
